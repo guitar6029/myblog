81a0ee4734bb890f0bcdfe26f23b5c9b8e63a3aa
Josh <jsbusiness305@gmail.com>
2025-02-19 14:21:58 -0500

    added DashboardLayout

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,7 @@
 export default function Dashboard(){
     return (
         <div className="min-h-sceen flex flex-col p-4">
-            <h1>Dashboard</h1>
+            <h1>Dashboard Main</h1>
         </div>
     )
 }
