0f52c115121a708315ce8d8b4d9060500eacdc2d
Josh <jsbusiness305@gmail.com>
2025-02-19 17:33:51 -0500

    installed bcrypt for hashing passwords , added user to to the collection, added validaitons and error message, created logic to connecto to db

--- src/actions/auth.ts
+++ src/actions/auth.ts
@@ -1,8 +1,18 @@
 "use server";
 
+import { getCollection } from "@/lib/db";
 import { RegisterFormSchema } from "@/lib/rules";
+import bcrypt from "bcrypt";
+import { redirect } from "next/navigation";
 
-export async function register(state, formData) {
+type FormData = {
+  email: string;
+  password: string;
+  confirmPassword: string;
+  get: (val: string) => string;
+}
+
+export async function register(state, formData: FormData) {
   const validateFields = RegisterFormSchema.safeParse({
     email: formData.get("email"),
     password: formData.get("password"),
@@ -15,5 +25,37 @@ export async function register(state, formData) {
     };
   }
 
-  console.log("formData", formData);
+  const userCollection = await getCollection("users");
+  console.log("userCollection", userCollection);
+  if (userCollection) {
+    const { email, password } = validateFields.data;
+    const user = await userCollection.findOne({ email: email });
+    if (!user) {
+      
+      // hash the password
+      const hashedPassword = await bcrypt.hash(password, 10);
+      
+      await userCollection.insertOne({ email: email, password: hashedPassword });
+
+      //create session todo
+
+
+      //redirect todo
+      redirect('/dashboard');
+
+
+    } else {
+      return {
+        errors: {
+          message: "Email already exists",
+        },
+      };
+    }
+  } else {
+    return {
+      errors: {
+        message: "Somehthing went wrong",
+      },
+    };
+  }
 }
---
+++ src/lib/db.ts
@@ -0,0 +1,31 @@
+import { MongoClient, ServerApiVersion } from "mongodb";
+
+if (!process.env.MONGO_URI) {
+  throw new Error("MONGO_URI is not defined");
+}
+
+const client = new MongoClient(process.env.MONGO_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function getDB(dbname: string) {
+  try {
+    await client.connect();
+    console.log("Connected successfully to Mongo DB");
+    return client.db(dbname);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export async function getCollection(collectionname: string) {
+  const db = await getDB("myblog_nextjs");
+  if (db) {
+    return db.collection(collectionname);
+  }
+  return null;
+}
