dcf153386b54b816d7d11fa3317ce52f3576bd4d
Josh <jsbusiness305@gmail.com>
2025-02-19 14:48:38 -0500

    updated Header styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,17 @@
-import Link from "next/link"
+import Link from "next/link";
 
 export default function Header() {
-    return (
-        <header className="flex flex-row items-center p-4 bg-neutral-900 gap-2 text-white">
-            <Link href="/">Home</Link>
-            <Link href="/dashboard">Dashboard</Link>
-            <Link href="/posts">Posts</Link>
-            <Link href="/register">Register</Link>
-            <Link href="/about">About</Link>
-        </header>
-    )
+  return (
+    <header className="flex flex-row justify-between items-center p-4 bg-neutral-900 gap-2 text-white">
+      <div className="flex flex-row items-center gap-4">
+        <Link href="/">Home</Link>
+        <Link href="/dashboard">Dashboard</Link>
+        <Link href="/posts">Posts</Link>
+        <Link href="/about">About</Link>
+      </div>
+      <div className="flex flex-row items-center gap-2">
+        <Link href="/register">Register</Link>
+      </div>
+    </header>
+  );
 }
