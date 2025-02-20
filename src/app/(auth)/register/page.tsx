dd5da73a71521bcafeef43dc4d3845bfd8b36deb
Josh <jsbusiness305@gmail.com>
2025-02-19 19:45:20 -0500

    added server only line to db file

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Link from "next/link";
-import { useActionState, useState } from "react";
+import { useActionState } from "react";
 import { register } from "@/actions/auth";
 
 
