9548cee42934fbd774b039b58f447a4264e2b42a
Josh <jsbusiness305@gmail.com>
2025-02-19 22:16:51 -0500

    removed comment from MyLink

--- src/components/Link/MyLink.tsx
+++ src/components/Link/MyLink.tsx
@@ -1,15 +1,19 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
-
 type LinkProps = {
-    linkPath: string, // Update 'linkpath' to 'linkPath'
-    text: string
-}
+  linkPath: string;
+  text: string;
+};
 
-export default function MyLink({linkPath, text} : LinkProps) {
-    const pathname = usePathname();
-    return (
-        <Link href={linkPath} className={`link-text ${pathname === linkPath ? "active" : ""}`}>{text}</Link>
-    )
+export default function MyLink({ linkPath, text }: LinkProps) {
+  const pathname = usePathname();
+  return (
+    <Link
+      href={linkPath}
+      className={`link-text ${pathname === linkPath ? "active" : ""}`}
+    >
+      {text}
+    </Link>
+  );
 }
